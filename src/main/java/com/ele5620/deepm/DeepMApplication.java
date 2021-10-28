0bca6744cdf8868417abfcff8c09609b2b6e1708
Ruojie Yu <yuruojie777@163.com>
2021-10-28 11:25:21 +0800

    create initial Springboot project

---
+++ src/main/java/com/ele5620/deepm/DeepMApplication.java
@@ -0,0 +1,13 @@
+package com.ele5620.deepm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepMApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ele5620/deepm/DeepMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ele5620.deepm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepMApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
